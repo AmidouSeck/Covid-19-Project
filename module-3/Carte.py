24711d274884ab252a318010278f1ef2b5bb72f6
dembadiack <dembadiack@outlook.com>
2021-05-21 10:28:46 +0000

    etite correction bug graphe

--- module-3/Carte.py
+++ module-3/Carte.py
@@ -42,8 +42,8 @@ class Carte:
         plt.draw()
 
     def plotMap(self):
-        file = os.path.join(os.getcwd(),"module-3","senegal_administrative", "senegal_administrative.shp")
-        cities_file = os.path.join(os.getcwd(),"module-3","senegal_administrative", "sn.csv")
+        file = os.path.join("senegal_administrative", "senegal_administrative.shp")
+        cities_file = os.path.join("senegal_administrative", "sn.csv")
         cities = pd.read_csv(cities_file)
         map = gpd.read_file(file)
         self.axis = map.plot(color='lightblue', figsize=(20, 20), linewidth=1, edgecolor="black",cmap='RdBu',scheme='quantiles')
--- module-3/Subgraph.py
+++ module-3/Subgraph.py
@@ -14,14 +14,20 @@ class Subgraph:
         self.title = title
         l = Loader()
         cas = l.afficher(f"select nbCas from localites inner join ligne_com_local using(id_localite) inner join communique using(date) where communique.date = '{self.annee}-{self.mois}-{self.jour}' and localites.nom = '{self.title}';")
-        self.addTitle(f"{self.title} : {cas[0][0]}")
+        if cas == []:
+            cas = 0
+        else:
+            cas = cas[0][0]
+        self.addTitle(f"{self.title} : {cas}")
         self.addButton()
 
 
 
     def drawPlot(self, event):
         l = Loader()
-        self.ax.plot(l.progressionParRegion(self.title))
+        result = l.progressionParRegion(self.title)
+        print(result[1])
+        self.ax.plot(result[0],result[1])
 
     def addTitle(self, title):
         self.ax.set_title(title)
